81ba7b9c6c729f23e59dc6a5717c1612d0240bca
hakaci <hakan.dervisoglu@protonmail.com>
2024-07-08 22:05:57 +0300

    Fibonacci non recursive

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ def fibonacci(n):
 
 # Enter nth fibonacci number
 # It struggels after 35ty
-print(fibonacci(37))
+# print(fibonacci(37))
+
+def fibonacci_not_recursive(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n+1):
+        a, b = b, a + b
+    return b
+
+# Enter nth fibonacci number
+# No wait time
+print(fibonacci_not_recursive(1000))
